c223a2f4e5ff727999a0fc0216add6ac74f1bcb5
Karanveer Singh Brar <singh.karanveer10@gmail.com>
2020-08-27 12:52:44 +0530

    Update ContentView.swift
    Some refactoring done

--- ZoomImage/ZoomImage/ContentView.swift
+++ ZoomImage/ZoomImage/ContentView.swift
@@ -16,25 +16,52 @@ struct ContentView: View {
     @State var previousDragged: CGSize = CGSize.zero
     var body: some View {
         GeometryReader {reader in
-            Image("pizza1delivery").resizable().scaledToFit().animation(.default).offset(x: self.draggedSize.width, y: 0).scaleEffect(self.scale).scaleEffect(self.isTapped ? 2 : 1, anchor: UnitPoint(x: self.pointTapped.x / reader.frame(in: .global).maxX, y: self.pointTapped.y / reader.frame(in: .global).maxY)).gesture(TapGesture(count: 2).onEnded({
+            Image("pizza1delivery")
+            .resizable()
+            .scaledToFit()
+            .animation(.default)
+            .offset(x: self.draggedSize.width, y: 0)
+            .scaleEffect(self.scale)
+            .scaleEffect(self.isTapped ? 2 : 1,
+             anchor: UnitPoint(
+              x: self.pointTapped.x / reader.frame(in: .global).maxX,
+              y: self.pointTapped.y / reader.frame(in: .global).maxY
+              ))
+             .gesture(TapGesture(count: 2)
+             .onEnded({
             self.isTapped = !self.isTapped
-        }).simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged({ (value) in
+        })
+        .simultaneously(with: DragGesture(minimumDistance: 0, coordinateSpace: .global)
+        .onChanged({ (value) in
             self.pointTapped = value.startLocation
-            self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: value.translation.height + self.previousDragged.height)
+            self.draggedSize = CGSize(
+                 width: value.translation.width + self.previousDragged.width,
+                 height: value.translation.height + self.previousDragged.height)
 //            print(value.startLocation)
         }).onEnded({ (value) in
 //            print(value.location)
-            let offsetWidth = (reader.frame(in: .global).maxX * self.scale - reader.frame(in: .global).maxX) / 2
+            let globalMaxX = reader.frame(in: .global).maxX
+            let offsetWidth = ((globalMaxX * self.scale) - globalMaxX) / 2
             let newDraggedWidth = self.draggedSize.width * self.scale
             if (newDraggedWidth > offsetWidth) {
-                self.draggedSize = CGSize(width: offsetWidth / self.scale, height: value.translation.height + self.previousDragged.height)
+                self.draggedSize = CGSize(
+                    width: offsetWidth / self.scale, 
+                    height: value.translation.height + self.previousDragged.height
+                    )
             } else if (newDraggedWidth < -offsetWidth) {
-                self.draggedSize = CGSize(width: -offsetWidth / self.scale, height: value.translation.height + self.previousDragged.height)
+                self.draggedSize = CGSize(
+                    width: -offsetWidth / self.scale, 
+                    height: value.translation.height + self.previousDragged.height
+                    )
             } else {
-                self.draggedSize = CGSize(width: value.translation.width + self.previousDragged.width, height: value.translation.height + self.previousDragged.height)
+                self.draggedSize = CGSize(
+                    width: value.translation.width + self.previousDragged.width, 
+                    height: value.translation.height + self.previousDragged.height
+                    )
             }
             self.previousDragged = self.draggedSize
-            }))).gesture(MagnificationGesture().onChanged({ (scale) in
+            }))).gesture(MagnificationGesture()
+            .onChanged({ (scale) in
             self.scale = scale.magnitude
         }).onEnded({ (scaleFinal) in
             self.scale = scaleFinal.magnitude
